1867e36858e1ba4f39d41e65af59551b5499dc18
Vinod Kumar <142583979+Vinodkumar8318@users.noreply.github.com>
2023-11-04 20:08:43 +0530

    Update PES_FA.v

--- PES_FA.v
+++ PES_FA.v
@@ -1,5 +1,5 @@
-For this project we ae not using Full adder and ripple carry adder as a two dseperate verilog file 
-   we are using only 1 verliog that is PES_RCA.v file which is there in the repo
+For this project we ae not using Full adder and ripple carry adder as a 2 Seperate verilog files. 
+  So We are using only 1 verliog and 1 Test bench file that is PES_RCA.v and PES_RCA_TB.v files which is there in the repo.
 
 
 // module PES_FA(A, B, clk, S, Cout);
